72bb6dd511726daf71dbbbdbf4e9da49b21cb747
Josua Martinez <martjosua9@gmail.com>
2026-04-29 18:25:07 -0500

    laboratorio3

---
+++ lab3/lab310.py
@@ -0,0 +1,5 @@
+# Usando isalpha(), isdigit() e isupper()
+palabra = "Python123"
+print(palabra.isalpha())
+print("123".isdigit())
+print("HOLA".isupper())
---
+++ lab3/lab39.py
@@ -0,0 +1,5 @@
+# Usando capitalize(), center() y count()
+texto = "hola mundo"
+print(texto.capitalize())
+print(texto.center(20, '*'))
+print(texto.count('o'))
